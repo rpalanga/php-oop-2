a0f20310fab3d06598d37c6ba5b21a61ad41392b
roberto-palanga <r.palanga94@gmail.com>
2024-04-03 16:29:22 +0200

    Strutturazione iniziale

--- Models/Product.php
+++ Models/Product.php
@@ -1,2 +1,21 @@
 <?php
 
+require './Food.php';
+require './Game.php';
+require './Kennel.php'
+
+class Product{
+    public $name;
+    public $image;
+    public $descritpion;
+    public $price;
+    
+    function __constrict($_name, $_image, $_description, $_price){
+        $this->name = $_name;
+        $this->image = $_image;
+        $this->descritpion = $_description;
+        $this->price = $_price;
+    }
+
+
+}
--- db.php
+++ db.php
@@ -1,4 +1,9 @@
 <?php
 
-require './Models/Product.php'
+require './Models/Product.php';
 
+
+
+$products[
+
+];
